3adc85306e028fc7f9a39969e542a560fb65838a
anthonyfc9 <44877558+anthonyfc9@users.noreply.github.com>
2019-08-26 20:16:58 -0400

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -3,50 +3,46 @@ let sonicPos = 1;
 let knucklesPos= 1;
 
 let eggmanPos=1;
+let eggman2=document.getElementById("eggman");
+let sonic2=document.getElementById("sonic");
+let knuckles2=document.getElementById("knuckles");
 
-const start =()=>{
-   
-let start=document.getElementById("start");
-
-eggman=document.getElementById("eggman");
-
-start.style.display= "none";
-
-eggman.style.display= "block";
-
-sonic.style.display= "block";
-
-knuckles.style.display= 'block';
-
-
-
-const addOne =()=>{
-
-eggmanPos++;
-
-eggman.style.marginleft= eggmanPos+'vw';
-
-if(eggmanPos==100 && sonicPos <100 && knucklesPos <100){if (confirm('you both let eggman get away!!!)')){location.reload();}}
+const start =() => {
+    let start2=document.getElementById("start");
+start2.style.display= "none";
+// start2.style.opacity = '0';
+eggman2.style.display= "block";
+sonic2.style.display= "block";
+knuckles2.style.display= 'block';
 
 }
 
-for(i=1;i<=100;i ++){
+// const addOne =()=>{
 
-window.setInterval(addOne(), 500);
+// eggmanPos++;
 
-}
+// eggman2.style.marginleft= eggmanPos+'vw';
 
-}
+// if(eggmanPos==100 && sonicPos <100 && knucklesPos <100){if (confirm('you both let eggman get away!!!)')){location.reload();}}
+
+// }
+
+// for(i=1;i<=100;i ++){
+
+// window.setInterval(addOne(), 500);
+
+// }
 
 
 
-const sonic =()=>{
 
-let sonic=document.getElementById("sonic");
+document.addEventListener('keydown',()=>{
 
-SonicPos+=1;
+    if(event.keyCode==83){
+console.log('oof')
+sonicPos+=1;
 
-sonic.style.marginLeft= sonicPos+'vw' ;
+sonic2.style.marginLeft= sonicPos+'vw' ;
 
 if (sonicPos==100){
 
@@ -54,23 +50,40 @@ if (confirm("Sonic has won the race!!")){location.reload();}
 
 }
 
-}
+    }})
 
 
 
-const knuckles =()=>{
 
-let knuckles=document.getElementById("knuckles");
+// const knuckles =()=>{
 
-knucklesPos+=1;
 
-knuckles.style.marginLeft= knucklesPos+'vw' ;
 
-if (knucklesPos==100){
+// knucklesPos+=1;
 
-if (confirm("Knuckles has won the game!!")){location.reload();}
+// knuckles2.style.marginLeft= knucklesPos+'vw' ;
 
-}
+// if (knucklesPos==100){
 
-}
+// if (confirm("Knuckles has won the game!!")){location.reload();}
 
+// }
+
+// }
+
+
+document.addEventListener('keydown',()=>{
+
+
+    if(event.keyCode==75){
+    knucklesPos+=1;
+    
+    knuckles2.style.marginLeft= knucklesPos+'vw' ;
+    
+    if (knucklesPos==100){
+    
+    if (confirm("Knuckles has won the game!!")){location.reload();}
+    
+    }
+    
+        }})
